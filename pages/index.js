b948c83f909017e48fa37e8188d6c04cd0aa0efb
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2023-11-28 16:49:17 +0000

    Modificando Frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Teste 1</h1>
+    return <h1>O sucesso nasce do querer, 
+        da determinação e persistência em se 
+        chegar a um objetivo. Mesmo não atingindo o alvo, 
+        quem busca e vence obstáculos, no mínimo fará coisas 
+        admiráveis.</h1>
 }
 
 export default Home;
+
