fceb4b2ed58001d1eff2144a25df68f3e7b595a8
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2023-11-30 16:20:09 +0000

    adicionando o `script lint:check e lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-function Home(){
-    return <h1>Quando nada parece dar certo, vou ver o cortador de pedras martelando sua rocha talvez 100 vezes, sem que uma única rachadura apareça. Mas na centésima primeira martelada a pedra se abre em duas, e eu sei que não foi aquela que conseguiu isso, mas todas as que vieram antes. S2</h1>
+function Home() {
+  return (
+    <h1>
+      Quando nada parece dar certo, vou ver o cortador de pedras martelando sua
+      rocha talvez 100 vezes, sem que uma única rachadura apareça. Mas na
+      centésima primeira martelada a pedra se abre em duas, e eu sei que não foi
+      aquela que conseguiu isso, mas todas as que vieram antes. S2
+    </h1>
+  );
 }
 
 export default Home;
-
