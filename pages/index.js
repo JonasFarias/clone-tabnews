8286f357b3c1d9fc4e2c5093b9b703d5b6047934
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2023-12-04 17:05:45 +0000

    Mudando frase da pagina em contrução

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Quando nada parece dar certo, vou ver o cortador de pedras martelando sua
-      rocha talvez 100 vezes, sem que uma única rachadura apareça. Mas na
-      centésima primeira martelada a pedra se abre em duas, e eu sei que não foi
-      aquela que conseguiu isso, mas todas as que vieram antes. S2
-    </h1>
-  );
+  return <h1>Em construção</h1>;
 }
 
 export default Home;
